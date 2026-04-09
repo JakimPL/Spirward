02d17d08eaa804627513725d09a3bac62fda3263
JakimPL <jakimpl@gmail.com>
2026-04-09 10:42:57 +0200

    Added: double rotation

--- core/spiral.asm
+++ core/spiral.asm
@@ -21,11 +21,11 @@ draw_spiral:
     pusha
 .increment_offset:
     fld1
-    fidiv word [focal_length] ; offset ← 1 / focal_length
+    fidiv word [focal_length]          ; offset_delta ← 1 / focal_length
     %ifdef DOS
     mov si, offset
     fadd dword [si]
-    fst dword [si]
+    fst dword [si]                     ; offset ← offset + offset_delta
     %endif
     %ifdef LINUX
     mov esi, offset
@@ -52,45 +52,45 @@ calculate_uv_values:
 .v:
     %ifdef DOS
     mov di, f_v
-    fst dword [di]           ; v ← offset
+    fst dword [di]                     ; v ← offset
     %endif
     %ifdef LINUX
     mov edi, f_v
-    fst dword [edi]          ; v ← offset
+    fst dword [edi]                    ; v ← offset
     %endif
 .v_step:
     fldpi
-    fidiv word [i]           ; v_step ← π / i
+    fidiv word [i]                     ; v_step ← π / i
 .v_step_2x:
     fld st0
-    fadd st0, st0            ; v_step_2x ← 2π / i
+    fadd st0, st0                      ; v_step_2x ← 2π / i
     fxch
 .u:
     fld st0
-    fimul word [focal_length] ; u ← v_step × focal_length
-; frndint ; snap to integer for a cylindrical effect
+    fimul word [focal_length]          ; u ← v_step × focal_length
+    frndint                            ; snap to integer for a cylindrical effect
 .u_int:
     fld st0
     fdiv dword [checkerboard_size]
-    fistp word [u_int]       ; checkerboard_u ← ⌊u / checkerboard_size⌋
+    fistp word [u_int]                 ; checkerboard_u ← ⌊u / checkerboard_size⌋
 .depth:
     fld st0
-    fmul st0, st0            ; depth ← u * u
+    fmul st0, st0                      ; depth ← u * u
 
 .calculate_light:
     fiadd word [attenuation_a]
     fidivr word [attenuation_b]
-    fistp word [light]       ; light ← b / (a + u * u)
+    fistp word [light]                 ; light ← b / (a + u * u)
 
 calculate_initial_point:
-    fadd st0, st3            ; u ← u + offset
+    fadd st0, st3                      ; u ← u + offset
 .u_sincos:
     fsincos
 .py:
-    fdiv st2                 ; py ← sin u / v_step
+    fdiv st2                           ; py ← sin u / v_step
 .px:
     fxch st2
-    fdivp st1                ; px ← cos u / v_step
+    fdivp st1                          ; px ← cos u / v_step
     fxch
     mov cl, byte [i]
 
@@ -110,20 +110,26 @@ do_v_step:
     fld st2
     %ifdef DOS
     fadd dword [di]
-    fst dword [di]           ; v ← v + 2 * v_step
+    fst dword [di]                     ; v ← v + 2 * v_step
     %endif
     %ifdef LINUX
     fadd dword [edi]
-    fst dword [edi]          ; v ← v + 2 * v_step
+    fst dword [edi]                    ; v ← v + 2 * v_step
     %endif
 .checkerboard_v:
     fld st0
     fdiv dword [checkerboard_size]
-    fistp word [v_int]       ; checkerboard_v ← ⌊v / checkerboard_size⌋
+    fistp word [v_int]                 ; checkerboard_v ← ⌊v / checkerboard_size⌋
 .increment_px_py:
+    %ifdef DOS
+    fadd dword [di]
+    %endif
+    %ifdef LINUX
+    fadd dword [esi]                   ; double v rotation
+    %endif
     fsincos
-    fsubp st3, st0           ; py ← py - sin(v)
-    fsubp st1, st0           ; px ← px - cos(v)
+    fsubp st3, st0                     ; py ← py - sin(v + offset)
+    fsubp st1, st0                     ; px ← px - cos(v + offset)
     fist word [px_int]
     fxch
     fist word [py_int]
