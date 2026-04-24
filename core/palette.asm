f8b77a9915d9d712cd615c1be0e89eb2a6529715
JakimPL <jakimpl@gmail.com>
2026-04-24 22:02:50 +0200

    Minor changes

--- core/palette.asm
+++ core/palette.asm
@@ -1,17 +1,17 @@
-%ifndef CORE_PALETTE_ASM
-%define CORE_PALETTE_ASM
+    %ifndef CORE_PALETTE_ASM
+    %define CORE_PALETTE_ASM
 
 palette_loop:
     mov al, bl
-    PALETTE_OUT 0                      ; R
+    PALETTE_OUT                        ; R
     add al, 0x08
     cmp al, MAX_COLOR
     jbe .palette_ok
     mov al, MAX_COLOR
 .palette_ok:
-    PALETTE_OUT 1                      ; G
-    PALETTE_OUT 2                      ; B
+    PALETTE_OUT                        ; G
+    PALETTE_OUT                        ; B
     inc bx
     loop palette_loop
 
-%endif
+    %endif
